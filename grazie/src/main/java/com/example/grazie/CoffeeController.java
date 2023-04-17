a72431024405558558cf555c37e61f1bf1a3eeb5
revatain <ireva234@naver.com>
2023-04-17 17:37:24 +0900

    04.17

--- grazie/src/main/java/com/example/grazie/CoffeeController.java
+++ grazie/src/main/java/com/example/grazie/CoffeeController.java
@@ -1,8 +1,24 @@
 package com.example.grazie;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 // 카운터 직원
 @RestController
+@RequiredArgsConstructor
 public class CoffeeController {
-    
+    private final CoffeeService coffeeService;
+    @GetMapping("/아메리카노")
+    public Coffee 아메리카노(){
+        return coffeeService.아메리카노();
+    }
+    @GetMapping("/카푸치노")
+    public Coffee 카푸치노(){
+        return coffeeService.카푸치노();
+    }
+    @GetMapping("/아샷추")
+    public Coffee 아샷추(){
+        return coffeeService.아샷추();
+    }
 }
