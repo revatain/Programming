e4aed9c7935fec992171984b64b04560c3bb19e0
revatain <ireva234@naver.com>
2023-04-24 11:01:04 +0900

    04.24

---
+++ my/src/main/java/com/example/my/todo/TodoController.java
@@ -0,0 +1,12 @@
+package com.example.my.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TodoController {
+    @GetMapping("/todoList")
+    public String todoList() {
+        return "todoList";
+    }
+}
