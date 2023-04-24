04f6ca398439712dbebb31e783c64f3710cd34e2
revatain <ireva234@naver.com>
2023-04-24 11:17:06 +0900

    0424

--- my/src/main/java/com/example/my/todo/TodoController.java
+++ my/src/main/java/com/example/my/todo/TodoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TodoController {
     @GetMapping("/todoList")
-    public String todoList() {
-        return "todoList";
+    public String todoList(){
+        return"todoList";
     }
 }
