1104f59a9752172967506a9da9c1bca320c843a9
revatain <ireva234@naver.com>
2023-05-01 10:45:10 +0900

    05.01

--- my/src/main/java/com/example/my/todo/controller/TodoControllerApiV1.java
+++ my/src/main/java/com/example/my/todo/controller/TodoControllerApiV1.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.my.todo.service.TodoService;
+import com.example.my.todo.service.TodoServiceApiV1;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,20 +12,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TodoControllerApiV1 {
     
-    private final TodoService todoService;
+    private final TodoServiceApiV1 todoServiceAp1V1;
 
     @GetMapping("/api/v1/insert")
     public void insert(@RequestParam String content) {
-        todoService.insert(content);
+        todoServiceAp1V1.insert(content);
     }
 
     @GetMapping("/api/v1/update")
     public void update(@RequestParam Integer idx) {
-        todoService.update(idx);
+        todoServiceAp1V1.update(idx);
     }
 
     @GetMapping("/api/v1/delete")
     public void delete(@RequestParam Integer idx) {
-        todoService.delete(idx);
+        todoServiceAp1V1.delete(idx);
     }
 }
--- my/src/main/java/com/example/my/todo/service/TodoService.java
+++ my/src/main/java/com/example/my/todo/service/TodoService.java
@@ -1,6 +1,5 @@
 package com.example.my.todo.service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -23,39 +22,5 @@ public class TodoService {
         return todoRepository.findByDeleteYn(deleteYn);
     }
 
-    public void insert(String content) {
-        TodoEntity todoEntity = TodoEntity.builder()
-            .content(content)
-            .doneYn('N')
-            .deleteYn('N')
-            .createDate(LocalDateTime.now())
-            .build();
-        todoRepository.insert(todoEntity);
-    }
-
-    public void update(Integer idx) {
-        TodoEntity todoEntity = todoRepository.findByIdx(idx);
-
-        if(todoEntity.getDoneYn().equals('N')){
-            todoEntity.setDoneYn('Y');
-        } else {
-            todoEntity.setDoneYn('N');
-        }
-        todoEntity.setUpdateDate(LocalDateTime.now());
-
-        todoRepository.update(todoEntity);
-
-    }
-
-    public void delete(Integer idx) {
-
-        TodoEntity todoEntity = todoRepository.findByIdx(idx);
-        todoEntity.setDeleteYn('Y');
-        todoEntity.setDeleteDate(LocalDateTime.now());
-
-        todoRepository.update(todoEntity);
-        
-    }
-
-
+   
 }
