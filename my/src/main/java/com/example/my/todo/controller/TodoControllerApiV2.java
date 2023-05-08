b4a71634689b1b69fec016015202ffabe9be1a10
revatain <ireva234@naver.com>
2023-05-08 12:32:10 +0900

    05.08

--- my/src/main/java/com/example/my/todo/controller/TodoControllerApiV2.java
+++ my/src/main/java/com/example/my/todo/controller/TodoControllerApiV2.java
@@ -15,7 +15,8 @@ import com.example.my.todo.dto.TodoDTO;
 import com.example.my.todo.service.TodoServiceApiV2;
 
 import lombok.RequiredArgsConstructor;
-
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/todo")
@@ -25,21 +26,25 @@ public class TodoControllerApiV2 {
 
     @GetMapping
     public ResDTO<?> select() {
+        log.info("리스트를 가져옵니다.");
         return todoServiceApiV2.findByDeleteYn('N');
     }
 
     @PostMapping
     public ResDTO<?> insert(@Validated @RequestBody TodoDTO.ReqBasic reqDto) {
+        log.info("할 일 (" + reqDto.getContent() + ") 추가를 요청합니다.");
         return todoServiceApiV2.insert(reqDto);
     }
 
     @PutMapping("/{idx}")
     public ResDTO<?> update(@PathVariable Integer idx) {
+        log.info(idx + "번 할 일 수정을 요청합니다.");
         return todoServiceApiV2.update(idx);
     }
 
     @DeleteMapping("/{idx}")
     public ResDTO<?> delete(@PathVariable Integer idx) {
+        log.warn(idx + "번 할 일 삭제를 요청합니다.");
         return todoServiceApiV2.delete(idx);
     }
 }
